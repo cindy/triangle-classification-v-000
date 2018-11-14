1f0145b691e3d19b592662a100d00fffaea341f9
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-14 05:20:27 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,12 +8,13 @@ class Triangle
 
   def kind
     values = [@a, @b, @c]
+    require 'pry'; binding.pry
     if  values == [@c] * 3
       return :equilateral
     elsif values.uniq.size == 2
       return :isosceles
     elsif values.include?(0)
-      require 'pry'; binding.pry
+      
       raise TriangleError
     else
       return :scalene
